74261fb20a70b96d6caa645e6f05fa890f91f980
soorajkj <soorajkj46@gmail.com>
2023-01-10 23:20:11 +0530

    chore: engine lock to yarn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ export default function HomePage() {
   return (
     <>
       <Head>
-        <title>Jotter.co | For prodcutive individuals</title>
-        <meta name="description" content="management tool for individuals" />
+        <title>Scribble</title>
+        <meta name="description" content="still in developement mode" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
