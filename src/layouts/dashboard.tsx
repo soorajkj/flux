a846f97cf3bf539f8eb15075244b731e26ebe1cd
soorajkj <soorajkj46@gmail.com>
2023-01-12 23:57:42 +0530

    feat:layout modules

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -1,14 +1,34 @@
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { cva, type VariantProps } from 'class-variance-authority';
+import { ReactNode, useState } from 'react';
+
+const sidebarStyles = cva(
+  'fixed top-0 h-full transition ease-in-out duration-300 lg:static z-50',
+  {
+    variants: {
+      open: {
+        true: 'left-0',
+        false: '-left-full',
+      },
+    },
+  }
+);
+
+interface DashboardLayoutProps extends VariantProps<typeof sidebarStyles> {
+  children: ReactNode;
+}
+
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  // eslint-disable-next-line no-unused-vars
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="h-screen w-screen overflow-hidden">
       <div className="flex h-full w-full flex-row">
-        <aside className="h-full w-auto">Sidebar</aside>
+        <aside className={sidebarStyles({ open })}>
+          <nav>Navbar</nav>
+        </aside>
         <div className="flex flex-1 flex-col">
-          <header>Header</header>
+          <header className="flex justify-between">Header</header>
           <main className="flex-grow overflow-y-auto">{children}</main>
           <footer>Footer</footer>
         </div>
