1a7092b1d9f4303e2070d68ffb3d237c7d02e9bd
soorajkj <soorajkj46@gmail.com>
2023-01-13 23:06:02 +0530

    layout modules

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -1,30 +1,14 @@
-import { cva, type VariantProps } from 'class-variance-authority';
-import { ReactNode, useState } from 'react';
+import { ReactNode } from 'react';
 
-const sidebarStyles = cva(
-  'fixed top-0 h-full transition ease-in-out duration-300 lg:static z-50',
-  {
-    variants: {
-      open: {
-        true: 'left-0',
-        false: '-left-full',
-      },
-    },
-  }
-);
-
-interface DashboardLayoutProps extends VariantProps<typeof sidebarStyles> {
+interface DashboardLayoutProps {
   children: ReactNode;
 }
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
-  // eslint-disable-next-line no-unused-vars
-  const [open, setOpen] = useState(false);
-
   return (
     <div className="h-screen w-screen overflow-hidden">
       <div className="flex h-full w-full flex-row">
-        <aside className={sidebarStyles({ open })}>
+        <aside className="h-full w-auto">
           <nav>Navbar</nav>
         </aside>
         <div className="flex flex-1 flex-col">
