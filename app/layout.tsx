e7ae93175d461ce598dc64d1ee072b22db967dc8
soorajkj <soorajkj46@gmail.com>
2023-01-15 22:59:51 +0530

    refactor: layout comp improvement

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,24 @@
+import { ReactNode } from 'react';
+import Navbar from '_components/navbar';
+import Header from '_components/header';
+import Footer from '_components/footer';
 import '_styles/tailwind.css';
 import '_styles/app.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="midnight scroll-smooth">
       <head />
-      <body>
+      <body className="relative font-sans text-size-sm font-weight-normal leading-snug">
         <div className="h-screen w-screen overflow-hidden">
           <div className="flex h-full w-full flex-row">
-            <aside className="h-full w-auto">Sidebar</aside>
+            <aside className="h-full w-auto">
+              <Navbar />
+            </aside>
             <div className="flex flex-1 flex-col">
-              <header>Header</header>
+              <Header />
               <main className="flex-grow overflow-y-auto">{children}</main>
-              <footer>Footer</footer>
+              <Footer />
             </div>
           </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
-export default function Page() {
-  return <div>Page</div>;
+export default function HomePage() {
+  return <div></div>;
 }
