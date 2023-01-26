b3338f0d92e5ecc671522bbb5dbe29d0ad75cc3a
soorajkj <soorajkj46@gmail.com>
2023-01-26 23:57:32 +0530

    style: add new colors to palette

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,3 @@
-import Checkbox from '_components/checkbox';
-import Radio from '_components/radio';
-
 export default function App() {
-  return (
-    <div>
-      <Checkbox />
-      <Checkbox />
-      <Checkbox />
-      <Checkbox defaultChecked />
-      <Checkbox disabled />
-      <Checkbox disabled defaultChecked />
-      <Radio name="rd" />
-      <Radio name="rd" />
-      <Radio name="rd" />
-      <Radio name="rd" />
-      <Radio name="rd" />
-      <Radio name="rd" disabled />
-      <Radio name="rd" defaultChecked disabled />
-    </div>
-  );
+  return <div></div>;
 }
