a91ac4fb433fb72117b2b5c8c48b03b4e3724132
soorajkj <soorajkj46@gmail.com>
2023-01-27 08:23:57 +0530

    style: update core components styles

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -2,7 +2,7 @@ import { cva, VariantProps } from 'class-variance-authority';
 import { forwardRef } from 'react';
 
 const CheckboxStyles = cva([
-  'checkbox appearance-none relative inline-block mx-1 my-1 w-4 h-4 border border-color-tertiary-dark rounded cursor-pointer select-none outline-none after:absolute after:top-1/2 after:left-1/2 after:w-1 after:h-2 after:rotate-45 after:-translate-x-1/2 after:-translate-y-2/3 after:scale-0 after:transition checked:bg-color-accent checked:border-color-accent checked:after:border-color-accent-contrast checked:after:scale-100 disabled:pointer-events-none disabled:bg-color-tertiary-light disabled:opacity-70 disabled:checked:bg-color-accent disabled:checked:border-color-accent disabled:checked:after:border-color-accent-contrast',
+  'checkbox appearance-none relative inline-block mx-1 my-1 w-4 h-4 border border-color-tertiary-dark rounded cursor-pointer select-none outline-none after:absolute after:top-1/2 after:left-1/2 after:w-1 after:h-2 after:rotate-45 after:-translate-x-1/2 after:-translate-y-2/3 after:scale-0 after:transition checked:bg-color-accent checked:border-color-accent checked:after:border-color-accent-contrast checked:after:scale-100 disabled:pointer-events-none disabled:bg-color-tertiary-light disabled:opacity-50 disabled:checked:bg-color-accent disabled:checked:border-color-accent disabled:checked:after:border-color-accent-contrast',
 ]);
 
 interface CheckboxProps
--- src/components/radio.tsx
+++ src/components/radio.tsx
@@ -1,7 +1,7 @@
 import { cva, VariantProps } from 'class-variance-authority';
 
 const RadioStyles = cva([
-  'radio appearance-none relative inline-block mx-1 my-1 w-4 h-4 bg-color-primary border border-color-tertiary-dark rounded-full cursor-pointer select-none outline-none after:absolute after:top-1/2 after:left-1/2 after:w-2 after:h-2 after:bg-color-tertiary after:rounded-full after:-translate-x-1/2 after:-translate-y-1/2 after:scale-0 after:transition checked:bg-color-accent checked:border-color-accent checked:after:scale-100 checked:after:bg-color-primary disabled:pointer-events-none disabled:opacity-70 disabled:bg-color-primary-dark disabled:checked:bg-color-accent',
+  'radio appearance-none relative inline-block mx-1 my-1 w-4 h-4 bg-color-primary border border-color-tertiary-dark rounded-full cursor-pointer select-none outline-none after:absolute after:top-1/2 after:left-1/2 after:w-2 after:h-2 after:bg-color-tertiary after:rounded-full after:-translate-x-1/2 after:-translate-y-1/2 after:scale-0 after:transition checked:bg-color-accent checked:border-color-accent checked:after:scale-100 checked:after:bg-color-primary disabled:pointer-events-none disabled:opacity-50 disabled:bg-color-primary-dark disabled:checked:bg-color-accent',
 ]);
 
 interface RadioProps
