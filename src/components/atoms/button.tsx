30a2e41ef3d3fbeed8dfaf3ab35720fb6a968c31
kjsooraj <sooraj.k@start-agile.com>
2023-02-02 17:10:18 +0530

    chore: add button component

---
+++ src/components/atoms/button.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { VariantProps, cva } from 'class-variance-authority';
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none data-[state=open]:bg-slate-100'
+);
+
+interface ButtonProps
+  extends React.DetailedHTMLProps<
+      React.ButtonHTMLAttributes<HTMLButtonElement>,
+      HTMLButtonElement
+    >,
+    React.AriaAttributes,
+    VariantProps<typeof buttonVariants> {}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const { className, ...rest } = props;
+
+    return (
+      <button ref={ref} className={buttonVariants({ className })} {...rest} />
+    );
+  }
+);
+
+Button.displayName = 'Button';
+
+export default Button;
