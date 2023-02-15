b8b8c7c9c89d83bafa1e047b73ce4f3db4d751e8
kjsooraj <sooraj.k@start-agile.com>
2023-02-15 18:17:34 +0530

    chore: downgrade to non-auth version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,15 @@
 import * as React from 'react';
-import { getServerSession } from 'next-auth';
 import { LayoutProps } from '@/types/layout';
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
-import { ThemeProvider } from '@/components/theme-provider';
-import SessionProvider from '@/components/session-provider';
-import Authentication from '@/components/authentication';
 import '@/styles/tailwind.css';
 import '@/styles/app.css';
 
 export default async function Layout(props: LayoutProps) {
   const { children } = props;
-  const session = await getServerSession(authOptions);
 
   return (
     <html lang="en">
       <head />
-      <body>
-        <ThemeProvider>
-          <SessionProvider session={session}>
-            {children}
-            <Authentication />
-          </SessionProvider>
-        </ThemeProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
