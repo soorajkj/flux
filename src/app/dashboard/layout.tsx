355ac76ddc4a9efc2c7c0e35d61f3f716baa99dd
soorajkj <soorajkj46@gmail.com>
2023-02-20 18:30:32 +0530

    chore: layout change

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,7 +2,6 @@ import { LayoutProps } from '@/types/layout';
 import Separator from '@/components/separator';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
-import Navbar from '@/components/navbar';
 
 export default function Layout(props: LayoutProps) {
   const { children } = props;
@@ -11,13 +10,11 @@ export default function Layout(props: LayoutProps) {
     <div className="h-screen w-screen overflow-hidden">
       <div className="flex h-full w-full flex-col">
         <Header />
-        <Separator />
-        <div className="flex flex-1 flex-row overflow-y-auto">
-          <Navbar />
-          <Separator orientation="vertical" />
-          <main className="flex-1">{children}</main>
-        </div>
-        <Separator />
+        <Separator orientation="horizontal" />
+        <main className="h-full w-full overflow-y-auto">
+          <div className="container">{children}</div>
+        </main>
+        <Separator orientation="horizontal" />
         <Footer />
       </div>
     </div>
--- src/components/navbar.tsx
+++
@@ -1,3 +0,0 @@
-export default function Navbar() {
-  return <nav className="h-full w-14"></nav>;
-}
