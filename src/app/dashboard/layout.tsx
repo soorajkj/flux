8d34609a1c125a7d6f5d4558f060f3e5a1fb7cb5
kjsooraj <sooraj.k@start-agile.com>
2023-02-24 09:45:37 +0530

    chore: update components naming conv

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import { LayoutProps } from '@/types/layout';
 
-export default function Layout(props: LayoutProps) {
+export default function DashboardLayout(props: LayoutProps) {
   const { children } = props;
 
   return <div className="h-screen w-screen overflow-hidden">{children}</div>;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,3 @@
-export default function Page() {
+export default function DashboardPage() {
   return <div>Dashboard Page</div>;
 }
