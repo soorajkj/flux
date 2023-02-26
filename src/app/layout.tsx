1cc8a0ba4cc24b29e4a6d07eacdce99e77c67580
soorajkj <soorajkj46@gmail.com>
2023-02-26 23:28:20 +0530

    chore: remove supabase support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,15 @@
-import 'server-only';
-import type { SupabaseClient } from '@supabase/auth-helpers-nextjs';
-import SupabaseProvider from '@/components/supabase-provider';
-import SupabaseListener from '@/components/supabase-listener';
-import { createServerClient } from '@/lib/server';
-import { LayoutProps } from '@/types/layout';
 import { inter } from '@/lib/fonts';
+import { LayoutProps } from '@/types/layout';
 import '@/styles/tailwind.css';
 import '@/styles/app.css';
 
-export type TypedSupabaseClient = SupabaseClient;
-export const revalidate = 0;
-
 export default async function AppLayout(props: LayoutProps) {
   const { children } = props;
-  const supabase = createServerClient();
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
 
   return (
     <html lang="en">
       <head />
-      <body className={inter.className}>
-        <SupabaseProvider session={session}>
-          <SupabaseListener token={session?.access_token} />
-          {children}
-        </SupabaseProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
