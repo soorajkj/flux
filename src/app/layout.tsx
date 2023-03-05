164e27a49267bf9f97e05b1c28e2549e371c7919
soorajkj <soorajkj46@gmail.com>
2023-03-05 12:14:52 +0530

    chore: next version -> 13.2.3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,20 @@
+import type { Metadata } from 'next';
 import { inter } from '@/lib/fonts';
 import { LayoutProps } from '@/types/layout';
 import '@/styles/tailwind.css';
 import '@/styles/app.css';
+
+export const metadata: Metadata = {
+  title: '',
+  description: '',
+  generator: 'Next.js',
+  applicationName: 'Next.js',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next', 'React', 'Typescript'],
+  creator: '',
+  publisher: '',
+  colorScheme: 'dark',
+};
 
 export default async function AppLayout(props: LayoutProps) {
   const { children } = props;
