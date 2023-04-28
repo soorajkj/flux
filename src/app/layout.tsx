ad9c18ba5f41039d5cfbb6252f0225db0c8a5012
soorajkj <soorajkj46@gmail.com>
2023-04-28 08:54:36 +0530

    chore: revert to default stylings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import { LayoutProps } from '@/types/layout';
-import { inter } from '@/libs/fonts';
 import '@/styles/tailwind.css';
 
 export default function AppLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body className={inter} style={{ fontFamily: 'var(--font-inter)' }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/libs/fonts.ts
+++
@@ -1,9 +0,0 @@
-import { Inter } from 'next/font/google';
-
-const interFont = Inter({
-  display: 'swap',
-  subsets: ['latin'],
-  variable: '--font-inter',
-});
-
-export const inter = interFont.variable;
