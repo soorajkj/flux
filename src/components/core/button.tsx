6d534798f41609092eba41a9dd7e359fa1567ecc
soorajkj <soorajkj46@gmail.com>
2023-04-29 15:13:31 +0530

    chore: add button, header, footer, container comps

---
+++ src/components/core/button.tsx
@@ -0,0 +1,39 @@
+import { VariantProps, cva } from 'class-variance-authority';
+import { forwardRef } from 'react';
+
+const buttonVariants = cva([
+  'relative',
+  'inline-flex',
+  'justify-center',
+  'items-center',
+  'text-center',
+  'text-sm',
+  'leading-none',
+  'rounded',
+  'py-2.5',
+  'px-4',
+  'cursor-pointer',
+  'select-none',
+  'focus-visible:outline-none',
+  'focus-visible:ring-2',
+  'focus-visible:ring-ring',
+  'focus-visible:ring-offset-2',
+  'disabled:opacity-50',
+  'disabled:pointer-events-none',
+]);
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <button ref={ref} className={buttonVariants({ className })} {...props} />
+    );
+  }
+);
+
+Button.displayName = 'Button';
+
+export { Button };
