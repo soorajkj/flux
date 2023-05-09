ffc352c4e0b596bba092e7d97f6770af32083ec5
kjsooraj <sooraj.k@start-agile.com>
2023-05-09 09:56:59 +0530

    refactor: cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,11 @@
 import { inter } from '@/libs/fonts';
-import { clsx } from '@/libs/utils';
 import { LayoutProps } from '@/types/layout';
 import '@/app/styles.css';
 
 export default function AppLayout({ children }: LayoutProps) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body
-        className={clsx(`
-        ${inter.className}
-        h-screen w-screen
-        select-none overflow-hidden
-        text-sm font-normal
-        leading-snug antialiased
-        dark:bg-zinc-900 dark:text-zinc-200`)}
-      >
+      <body className={inter.className}>
         <div className="flex h-full w-full flex-col">{children}</div>
       </body>
     </html>
