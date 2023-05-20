e397907831af3a05adf08c6ccd4f6ec3baca9cbb
soorajkj <soorajkj46@gmail.com>
2023-05-20 18:25:40 +0530

    refactor: minior edit on site layout

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { LayoutProps } from "@/types/layout";
 import ThemeProvider from "@/components/providers/theme";
 import "../../styles/tailwind.css";
 
-interface SiteLayoutProps extends LayoutProps {}
-
-export default function ApptLayout({ children }: SiteLayoutProps) {
+export default function SiteLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <body className="h-full min-h-screen w-full font-family-inter text-sm font-normal leading-normal antialiased">
