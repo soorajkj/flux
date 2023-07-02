274a66312bc8fbda1ab4d8965a99b56b07b23a2d
kjsooraj <sooraj.k@strat-agile.com>
2023-07-02 13:30:01 +0530

    refactor: home page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import ThemeProvider from "~components/providers/theme-provider";
-import { Pannel } from "~components/core/pannel";
+// import { Pannel } from "~components/core/pannel";
 import { Separator } from "~components/core/separator";
 import Header from "~components/header";
 import Footer from "~components/footer";
@@ -16,22 +16,16 @@ export default function Layout(props: LayoutProps) {
     <html lang="en" className={inter.variable} suppressHydrationWarning>
       <body className="h-full min-h-screen w-full bg-neutral-900 font-family-inter text-sm font-normal leading-normal text-neutral-500 antialiased">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="flex h-full min-h-screen flex-col">
-            <Pannel>
-              <Header />
-            </Pannel>
-            <Separator />
-            <div className="flex flex-1 flex-row">
-              <Pannel orientation="vertical" className="h-auto overflow-y-auto">
-                <Routesbar />
-              </Pannel>
+          <div className="flex h-full min-h-screen w-full flex-col">
+            <Header />
+            <Separator orientation="horizontal" />
+            <div className="flex h-full w-full flex-1 flex-row">
+              <Routesbar />
               <Separator orientation="vertical" className="h-auto" />
               <main className="flex-1">{children}</main>
             </div>
-            <Separator />
-            <Pannel>
-              <Footer />
-            </Pannel>
+            <Separator orientation="horizontal" />
+            <Footer />
           </div>
         </ThemeProvider>
       </body>
