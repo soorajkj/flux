f68e35a5323f9e784e59e589626b664764a5ac6c
kjsooraj <sooraj.k@strat-agile.com>
2023-10-23 17:27:44 +0530

    fix(svgr/webpack): next latest version breaks

--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"],
     });
     return config;
---
+++ src/components/core/icon.tsx
@@ -0,0 +1,31 @@
+import React, { forwardRef } from "react";
+import * as VisuallyHiddenPrimitive from "@radix-ui/react-visually-hidden";
+import Demo from "public/demo.svg";
+
+const icons = {
+  demo: Demo,
+};
+
+export interface IconProps extends React.SVGAttributes<SVGSVGElement> {
+  icon: keyof typeof icons;
+  label?: string;
+}
+
+const Icon = forwardRef<SVGSVGElement, IconProps>((props, ref) => {
+  const { icon, label, ...rest } = props;
+
+  const IconEle = icons[icon];
+
+  return (
+    <React.Fragment>
+      <IconEle ref={ref} width={16} height={16} {...rest} />
+      {label && (
+        <VisuallyHiddenPrimitive.Root>{label}</VisuallyHiddenPrimitive.Root>
+      )}
+    </React.Fragment>
+  );
+});
+
+Icon.displayName = "Icon";
+
+export { Icon };
