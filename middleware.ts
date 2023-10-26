f774b37cda8f74fc2d5672a057f7c5836c1a4c7c
kjsooraj <sooraj.k@strat-agile.com>
2023-10-26 21:55:08 +0530

    chore(supabase): remove supabase from application

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
-  await supabase.auth.getSession();
-  return res;
-}
