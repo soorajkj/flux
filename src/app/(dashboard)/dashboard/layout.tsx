b119c83bd4c1b01cf40b0260192c55f42d7ecad2
soorajkj <soorajkj46@gmail.com>
2023-11-22 14:54:33 +0530

    feat: base dashbaord layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,9 +1,21 @@
 import { LayoutProps } from "~types/layout";
+import { Separator } from "~components/core/separator";
+import Footer from "~components/footer";
+import Header from "~components/header";
+import Routesbar from "~components/routesbar";
 
-export default function Layout(_props: LayoutProps) {
+export default function Layout(props: LayoutProps) {
   return (
     <div className="flex h-screen w-full flex-col overflow-hidden">
-      {_props.children}
+      <Header />
+      <Separator orientation="horizontal" />
+      <div className="flex flex-1">
+        <Routesbar />
+        <Separator orientation="vertical" className="hidden lg:block" />
+        <main className="flex-1">{props.children}</main>
+      </div>
+      <Separator orientation="horizontal" />
+      <Footer />
     </div>
   );
 }
