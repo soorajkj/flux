16384e9ad352c76a9c6f0992aa7ef26d19c5f363
soorajkj <soorajkj46@gmail.com>
2024-02-11 15:20:31 +0530

    chore: temp branch

---
+++ src/components/_www/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="relative bg-zinc-50 text-zinc-700">
+      <div className="container mx-auto w-full max-w-screen-lg px-4"></div>
+    </footer>
+  );
+}
--- src/components/footer.tsx
+++
@@ -1,3 +0,0 @@
-export default function Footer() {
-  return <footer className="tweede-footer h-8"></footer>;
-}
