df6151c2e35b999d116cfa5ef5c52b37e2a2bead
soorajkj <soorajkj46@gmail.com>
2024-02-21 17:25:53 +0530

    fix: :ambulance: production break

--- src/app/(auth)/auth/callback/route.ts
+++ src/app/(auth)/auth/callback/route.ts
@@ -5,13 +5,11 @@ export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
   const origin = requestUrl.origin;
-  const next = requestUrl.searchParams.get("next") ?? "/";
+  const next = requestUrl.searchParams.get("next") ?? "/dashboard";
 
   if (code) {
     const supabase = createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) return NextResponse.redirect(`${origin}${next}`);
   }
-
-  // return NextResponse.redirect(`${origin}/dashboard`);
 }
