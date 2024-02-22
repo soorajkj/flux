f4607e7e2c046bfdaa4adfeab87693ac4924f650
soorajkj <soorajkj46@gmail.com>
2024-02-22 22:56:59 +0530

    refactor: :beers: www route design

--- src/components/_www/header.tsx
+++ src/components/_www/header.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import Button from "~components/core/button";
+import Icon from "~components/core/icon";
 import Separator from "~components/core/separator";
 import Logo from "~components/logo";
 
@@ -8,15 +11,25 @@ export default function Header() {
     <header className="sticky top-0 z-50 bg-neutral-900/60 backdrop-blur-md">
       <div className="container mx-auto w-full max-w-screen-lg px-4">
         <div className="flex items-center py-4">
-          <Logo href={"/"} scroll={true} />
-          <p className="ml-6">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          </p>
+          <Logo href="/" scroll />
+          <Button
+            className="ml-6 inline-flex h-9 cursor-pointer select-none items-center justify-center space-x-1 rounded-md"
+            unstyled
+          >
+            <Icon
+              icon="github"
+              height={24}
+              width={24}
+              className="fill-neutral-600"
+              label="github.com"
+            ></Icon>
+            <span className="text-xs">20.2k</span>
+          </Button>
           <div className="hidden flex-1 justify-end gap-4 md:flex">
-            <Button variant="ghots">Request a demo</Button>
+            <Button variant="ghots">Try Tesseract Free</Button>
             <Separator orientation="vertical" className="h-auto" />
             <Button variant="primary" asChild>
-              <Link href={"/signin"}>Login to your account</Link>
+              <Link href="/signin">Login to App</Link>
             </Button>
           </div>
         </div>
--- src/components/_www/hero.tsx
+++ src/components/_www/hero.tsx
@@ -3,39 +3,42 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import people from "public/images/people.webp";
+import people from "public/images/dashboard.png";
 import Button from "~components/core/button";
 
 export default function Hero() {
   return (
     <section className="relative">
       <div className="container mx-auto w-full max-w-screen-lg px-4">
-        <div className="relative z-20 mx-auto flex w-full max-w-2xl flex-col items-center pb-24 pt-12 text-center">
-          <div className="relative hidden w-full overflow-hidden">
+        <div className="relative mx-auto flex w-full max-w-2xl flex-col items-center pb-24 pt-12">
+          <div className="relative z-10 w-full overflow-hidden after:absolute after:bottom-0 after:left-0 after:right-0 after:top-0 after:bg-gradient-to-b after:from-transparent after:via-neutral-900 after:to-neutral-900 after:bg-contain">
             <Image
               src={people.src}
               alt="people rally image"
-              height={400}
+              height={300}
               width={600}
               priority={true}
-              style={{ width: "auto" }}
+              style={{ width: "auto", animationDuration: "60000ms" }}
+              className="relative animate-spin"
             ></Image>
           </div>
-          <h1 className="my-8 font-family-caprasimo text-4xl text-neutral-300 md:text-5xl md:leading-tight">
-            Organize, prioritize, and get things done.
-          </h1>
-          <p className="text-base">
-            Tesseract is the personalized and flexible writing / todo app that
-            adapts to the way you think. Streamline your work by doing it, and
-            seeing it, in one place.
-          </p>
-          <div className="mt-12 flex items-center gap-4">
-            <Button variant="default" asChild>
-              <Link href={"/signup"} scroll={true}>
-                Try Tesseract free
-              </Link>
-            </Button>
-            <Button variant="ghots">Request a demo</Button>
+          <div className="relative z-20 -mt-96 flex flex-col items-center text-center">
+            <h1 className="my-8 font-family-caprasimo text-4xl text-neutral-300 md:text-5xl md:leading-tight">
+              Organize, prioritize, and get things done.
+            </h1>
+            <p className="text-base">
+              Tesseract is the personalized and flexible writing / todo app that
+              adapts to the way you think. Streamline your work by doing it, and
+              seeing it, in one place.
+            </p>
+            <div className="mt-12 flex items-center gap-4">
+              <Button variant="default" asChild>
+                <Link href={"/signup"} scroll>
+                  Try Tesseract free
+                </Link>
+              </Button>
+              <Button variant="subtile">Request a demo</Button>
+            </div>
           </div>
         </div>
       </div>
